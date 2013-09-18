d855d49773fdc76cf2a44284419a583e1afa8b2e
Justin Horowitz <justin.horowitz@gmail.com>
2013-09-18 09:31:47 -0500

    Started to set up forward Burdet extraction

--- matlab/armdynamicsBurdetReflexes.m
+++ matlab/armdynamicsBurdetReflexes.m
@@ -1,6 +1,6 @@
-function [dqi]=armdynamicsInvertedBurdetReflexes(t,qi)
+function [dqi]=armdynamicsBurdetReflexes(t,qi)
 
-global measuredVals measuredTime errorVals errorTime kpgain
+global measuredVals measuredTime errorVals errorTime jointTorques kpgain
 
 lqi=length(qi);
 
@@ -23,13 +23,14 @@ omega_desired=qi(lqi/2+1:end);
 [D_expected,C_expected]=computeDC(theta_desired,omega_desired);
 
 kp0=[10.8 2.83; 2.51 8.67];
-joint_torques=abs(D_real*alpha_real+C_real+torque_outside);
+% In inverse version, the inertial and outside torques have already been measured
+%joint_torques=abs(D_real*alpha_real+C_real+torque_outside);
 kp1=[3.18*joint_torques(1) 2.15*joint_torques(2); 2.34*joint_torques(1) 6.18*joint_torques(2)];
 kp=kpgain*(kp0+kp1);
 
 torque_fb=kp*((theta_real-theta_desired) + (1/12)*(omega_real-omega_desired))+(kp/50)*(reflexE+2*reflexV);
 
-% Update the change in desired state
+%Update the change in desired state
 dqi=[omega_desired;
     D_expected\(D_real*alpha_real+torque_fb+torque_outside+C_real-C_expected);];  %If torque_fb and torque_outside=0, and c_real ~ c_expected, alpha = alpha desired.
 end
