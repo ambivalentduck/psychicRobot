68a0d3a9b7fa18e4ca40a816237d5802bc650de4
Justin Horowitz <justin.horowitz@gmail.com>
2013-11-04 11:42:42 -0600

    Updated to make boxplots

--- matlab/oldData.m
+++ matlab/oldData.m
@@ -6,8 +6,10 @@ global kpgain
 kpgain=1;
 gray=.7;
 vecmag_scale=.125;
+starts={};
+ends={};
 
-for k=5 %1:3 %8
+for k=1:8
     name=num2str(k);
     if exist(['../Data/curlkick',name,'.mat'],'file')
         load(['../Data/curlkick',name,'.mat']);
@@ -250,7 +252,7 @@ for k=5 %1:3 %8
         end
         drawnow
     end
-    %load(['../Data/curlkick',name,'.mat']);
+    save(['../Data/curlkick',name,'g.mat'],'trials');
 
     set(gcf,'Position',[66 1 1215 945]);
     hbottom=subplot(NSP,1,NSP)
@@ -295,14 +297,11 @@ for k=5 %1:3 %8
     title(['Histogram of subunit count. Sub #',num2str(k)])
     ylabel('Relative Frequency')
     
-    return
-    
-    starts={};
-    ends={};
     targdists=vecmag([targlocs(:,1)-targlocs(center,1) targlocs(:,2)-targlocs(center,2)]);
     nl=[trials(f).nlumps];
+    ip=[trials(f).isproblem];
     for c=1:5
-        f2=find(nl>=c);
+        f2=find((nl>=c)&~ip);
         starts{k,c}=zeros(length(f2),3);
         ends{k,c}=zeros(length(f2),3);
         for cc=1:length(f2)
@@ -313,8 +312,83 @@ for k=5 %1:3 %8
             ends{k,c}(cc,1:3)=[trials(kk).cumdist(inds(end))/rat trials(kk).yurot(inds(end),1)/rat trials(kk).tcat(inds(end))];
         end
     end
-    
 end
+
+submax=8;
+
+
+
+subnums={};
+for k=1:8
+    for c=1:5
+        starts{k,c}=starts{k,c}';
+        ends{k,c}=ends{k,c}';
+        subnums{k,c}=k*ones(size(starts{k,c}));
+    end
+end
+
+figure(666)
+clf
+subplot(5,3,14)
+nlumps=zeros(length(nlumps),submax);
+groups=nlumps;
+for k=1:submax
+    load(['../Data/curlkick',num2str(k),'g.mat']);
+    nlumps(:,k)=[trials.nlumps]';
+    groups(:,k)=ones(45,1)*k;
+    names{k+1}=num2str(k);
+end
+names{1}='All';
+groups2=[groups(:); 0*groups(:)];
+nlumps2=[nlumps(:); nlumps(:)];
+%boxplot(nlumps2,groups2,'orientation','horizontal','notch','on','labels',names)
+boxplot(nlumps2,groups2,'orientation','vertical','notch','on','labels',names)
+%boxplot(nlumps2,groups2,'orientation','vertical','plotstyle','compact','labels',names)
+xlabel('Subject Number')
+ylabel('Submovements Detected')
+
+yl=[3 1.5 2.5];
+tits={'Cumulative Distance Ratio','Progress Ratio','Time, s'};
+pos=0:8;
+pos=[pos-.1 pos+.1];
+
+cols=zeros(18,3);
+% for k=1:18
+%     if mod(k,2)
+%         cols(k,:)=[0 0 1];
+%     else
+%         cols(k,:)=[1 0 0];
+%     end
+% end
+cols(1:9,3)=1;
+cols(10:18,1)=1;
+
+for k=1:4
+    dats=[starts{:,k}]';
+    date=[ends{:,k}]';
+    sms=[subnums{:,k}]';
+    for met=1:3
+        subplot(5,3,met+3*(k-1))
+        dat2=[dats(:,met);dats(:,met);date(:,met);date(:,met)];
+        f=find((~imag(dat2))&(dat2<50));
+        sms2=[sms(:,met);0*sms(:,met);sms(:,met)+9;0*sms(:,met)+9];
+        boxplot(dat2(f),sms2(f),'orientation','vertical','plotstyle','compact','positions',pos,'colors',cols)
+        if k==1
+            title(tits{met})
+        end
+        if met==1
+            ylabel(['Submovement ',num2str(k)])
+        else
+            ylabel('')
+        end
+        ylim([0 yl(met)])
+        set(gca,'xtick',0:8)
+        set(gca,'xticklabel',names)
+    end
+end
+    
+   
+
 
 
 
