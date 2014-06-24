974eb8280bcd84dfc4fa4674dee4e5129220ddfa
Justin Horowitz <justin.horowitz@gmail.com>
2014-06-24 10:54:56 -0500

    Updated for work from home.

--- Paper2/updatedExtract/chiariLumps.m
+++ Paper2/updatedExtract/chiariLumps.m
@@ -1,6 +1,7 @@
 clc
 clear all
 load('pulse1.mat');
+close all
 
 N=14;
 onset=find(vecmag(trials(N).v)>.05,1,'first');
@@ -19,3 +20,35 @@ clf
 plot(distrem,distcov,'-x')
 
 %[distrem' ones(4,1)]\distcov'
+
+%% Step 1: Guess at a peak and do serial decomposition
+
+figure(1)
+clf
+hold on
+
+t=trials(N).t;
+lT=length(t);
+y=[trials(N).x trials(N).v];
+plot(1:lT,vecmag(y(:,3:4)));
+
+%next=168; % 210 Pretty fractal breakdown
+[blah,next]=max(vecmag(y(:,3:4)))
+first=sum(vecmag(y(:,3:4)));
+resid=y;
+
+cycles=0;
+while cycles<5
+    cycles=cycles+1
+    [lump,resid]=rulesFindLumps(t,resid,next);
+    if length(lump.t)==1
+        break
+    end
+    vm=vecmag(resid(:,3:4));
+    c=rand(3,1);
+    plot(1:lT,vm,'-','color',c);
+    plot(lump.inds,vecmag(lump.y(:,3:4)),'.','color',c);
+    [blah,next]=max(vm);
+end
+xlabel('Time, Samples (rate=200 Hz)')
+ylabel('Speed, meters/sec')
