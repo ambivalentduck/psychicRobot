8d9cfc212046b187ae15754d8bfe5d844889ae0a
Sajet <tejasmadhavan@gmail.com>
2014-07-08 11:27:27 -0500

    changed addsubject and created multidisp
    addsubject --> added an additional if - elseif statment that works with
    the input the user gave. if the user gave a string then the function
    will have the function getSubjectParams search for that string. IF the
    user gives something else (an integer) then the function turns that
    integer into a string and has the function getSubjectParams search for
    that particular subject number instead
    ALSO: commented code out at the bottom just before the function saves
    the information into a .mat file
    
    mulidisp --> this is a function that will compare the errors to the
    trial number of baseline, extraction, and washout phases and will stack
    them for all 8 patients

---
+++ internStuff/fJRBHCD.m
@@ -0,0 +1,8 @@
+function out=fJRBHCD(theta)
+theta1=theta(1);
+theta2=theta(2);
+fJ11=- (381.*sin(theta1 + theta2))./1250 - (317.*sin(theta1))./1000;
+fJ21=(381.*cos(theta1 + theta2))./1250 + (317.*cos(theta1))./1000;
+fJ12=-(381.*sin(theta1 + theta2))./1250;
+fJ22=(381.*cos(theta1 + theta2))./1250;
+out=[fJ11,fJ12;fJ21,fJ22];
