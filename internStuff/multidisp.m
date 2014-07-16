7dbe592391c03970b7483e38f6b1048a01e4eee3
Sajet <tejasmadhavan@gmail.com>
2014-07-16 16:03:56 -0500

    Update on multdisp
    Made the marker size to .01
    Made the line width to 2 for average, hand, and cursor

--- internStuff/multidisp.m
+++ internStuff/multidisp.m
@@ -17,7 +17,7 @@ phases=phases==1;
 % builds one display for all of the folloing data
 
 titles={'Max Perp','RMS','Straightness'};
-spacer=[.2 450 .14];
+spacer=[.2*.5 450*.5 .14*.5];
 
 SHIFT=.3;
 
@@ -26,27 +26,56 @@ cursor(:,2)=force(:,2)./cursor(:,2);
 
 summaryMet=@median;
 
+% The figure that will display all three plots on one figure 
+figure(1) 
+% sets the background to white
+set(gcf,'color','w')
+clf
 for N=1:3
-    figure(N)
-    clf
-    hold on
+%     figure(N)
+%     clf
+%     hold on
 
 
     for S = 1:8
         col=[rand(1,2) 0];
         dcol=[col(1:2) 1];
-
-        plot(allT,cursor(:,N)+S*spacer(N),'.','color',col)
-        plot([2*96+1,3*96],summaryMet(cursor(phases(:,3),N))+[0 0]+S*spacer(N),'-','color','b')
+        
+%         subplot iteration step
+        subplot(1,3,N);
+        
+%         Plot what you need
+        hold on
+        plot(allT,cursor(:,N)+S*spacer(N),'.','color',col,'MarkerSize',.01)
+        plot([2*96+1,3*96],summaryMet(cursor(phases(:,3),N))+[0 0]+S*spacer(N),'-','color','b','linewidth',2)
         plot(allT(phases(:,3)),hand(phases(:,3),N)+S*spacer(N),'x','color',dcol)
-        plot([2*96+1,3*96],summaryMet(hand(phases(:,3),N))+[0 0]+S*spacer(N),'-','color','k')
-        plot([97 4*96],summaryMet(cursor(phases(:,2)|phases(:,4),N))+[0 0]+S*spacer(N),'-.','color','r')
-
+        plot([2*96+1,3*96],summaryMet(hand(phases(:,3),N))+[0 0]+S*spacer(N),'-','color','k', 'linewidth',2)
+        plot([97 4*96],summaryMet(cursor(phases(:,2)|phases(:,4),N))+[0 0]+S*spacer(N),'-.','color','r','linewidth',2)
+        
+%         sets the appropriate labels depending on the current graph
+        if N == 1
+%             Max perpendicular distance
+            xlabel('Trials')
+            ylabel('Meters')
+        elseif N == 2
+%             RMS
+            xlabel('Trials')
+            ylabel('Newtons per Meter')
+        elseif N == 3
+%             Straightness 
+            xlabel('Trials')
+            ylabel('Meters')
+        end 
+        
+        
+        
         %    errave = (mean(errors(phase2(curs0),1))+mean(errors(phase4(curs0),1)))/2;
         %    plot([97 4*96],errave*[1 1]+num*.2,'--','color','red')
         %axis ([0 5*96 0 .2])
     end
     title(titles{N})
+    
+%     Graphs the verticle lines that separate the 5 phases 
     for g=1:4
         line([g*96,g*96],ylim)
     end
