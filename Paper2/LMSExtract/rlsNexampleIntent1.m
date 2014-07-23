8b0877314f2eee1940aa437c517e4f3e759ca513
Justin Horowitz <justin.horowitz@gmail.com>
2014-07-23 10:54:09 -0500

    RLS for meeting.

--- Paper2/LMSExtract/rlsNexampleIntent1.m
+++ Paper2/LMSExtract/rlsNexampleIntent1.m
@@ -15,10 +15,10 @@ intent=sin(1:N); %cos(12.7*(1:N))+5;
 y=Y+S2N*std(Y)*randn(1,N)+intent;
 linRegress=(x'\y')' %#ok<*NOPTS>
 
-w=zeros(M,N);
-lambda=1;
-P=zeros(M,M,N);
-P(:,:,1)=eye(M);
+w=zeros(M+1,N);
+lambda=diag([1.1*ones(1,3) 1.5]);
+P=zeros(M+1,M+1,N);
+P(:,:,1)=eye(M+1);
 
 c=2;
 
@@ -28,7 +28,7 @@ while c<=N
 %    
 %     w(:,c)=w(:,c-1)+k*alpha;
 %     P(:,:,c)=lambda*P(:,:,c-1)-lambda*k*x(:,c)'*P(:,:,c-1);
-    [w(:,c),P(:,:,c)]=RLS(x(:,c),y(:,c),w(:,c-1),P(:,:,c-1),lambda);
+    [w(:,c),P(:,:,c)]=RLS([x(:,c);1],y(:,c)',w(:,c-1),P(:,:,c-1),lambda);
     
     c=c+1;
 end
@@ -37,7 +37,7 @@ w_end=w(:,end)'
 close all
 figure(1)
 subplot(3,1,1)
-yp=sum(w.*x);
+yp=sum(w.*[x;ones(1,N)]);
 plot(1:N,y,'r',1:N,Y,'b',1:N,yp,'g')
 legend('With noise','Pure','Predicted')
 
--- Paper2/RLStoyExtract/rlsMain.m
+++ Paper2/RLStoyExtract/rlsMain.m
@@ -1,5 +1,6 @@
 clc
 clear all
+close all
 
 global M B
 
@@ -8,8 +9,8 @@ tspan=.5;
 tinc=.005;
 ta=(0:tinc:tspan)/tspan;
 
-xi=[1; 0];
-xf=[0; 0];
+xi=[1.1; .1];
+xf=[.26; .1];
 
 xd=(xi*ones(size(ta))+(xf-xi)*(10*ta.^3-15*ta.^4+6*ta.^5))';
 vd=((xf-xi)*(30*ta.^2-60*ta.^3+30*ta.^4)/tspan)';
@@ -21,25 +22,35 @@ ad=((xf-xi)*(60*ta-180*ta.^2+120*ta.^3)/(tspan^2))';
 
 t=0:tinc:1.5;
 Xd=[[xd vd ad];zeros(length(t)-length(ta),6)];
-Xd(:,1)=Xd(:,1)+1;
-F=zeros(length(t),2);
-F((t>.1)&(t<.2),1)=-10;
-K=15*ones(length(t),2);
+Xd(length(ta)+1:length(t),1)=Xd(length(ta)+1:length(t),1)+xf(1);
+Xd(length(ta)+1:length(t),2)=Xd(length(ta)+1:length(t),2)+xf(2);
+[Fb,Fa]=butter(10,3*tinc,'low');
+F=[7*filtfilt(Fb,Fa,randn(2*length(t),1)) 7*filter(Fb,Fa,randn(2*length(t),1))];
+F=F(end-length(t)+1:end,:);
 
-figure(1)
-clf
-hold on
-plot(t,Xd(:,1),'g')
+%F=zeros(length(t),2);
+%F((t>.1)&(t<.2),:)=-15;
+K=15*ones(length(t),2);
 
 M=.41;
 B=2.3;
 
 Xh=forward(t,Xd,F,K);
-plot(t,Xh(:,1),'k')
-
 Xx=reverse(t,Xh,F,K);
-plot(t,Xx(:,1),'r.')
 
+figure(1)
+clf
+subplot(2,1,1)
+hold on
+plot(Xd(:,1),Xd(:,2),'g')
+plot(Xh(:,1),Xh(:,2),'k')
+axis equal
+
+subplot(2,1,2)
+hold on
+plot(t,Xd(:,1),'g')
+plot(t,Xh(:,1),'k')
+plot(t,Xx(:,1),'r.')
 arrow([t' Xh(:,1)],[t' Xh(:,1)]+.05*[0*t' F(:,1)],.6*[1 1 1],.2);
 
 title('Proof of Tautology Being Intact')
@@ -47,28 +58,21 @@ xlabel('Time, Seconds')
 ylabel('Displacement in X')
 legend('Ground Truth Intention','Hand (Stiffness=15)','Intention (Stiffness=15)','Forces')
 
-%% Sobol spanned, short-time extraction bursts
+%% Sobol spanned extractions
 
 sParams=1;
-nCandidates=5;
-Kml=16;
+nCandidates=10;
+Kml=15.72;
 Kspan=.2;
 
 p=sobolset(sParams,'Skip',1e3,'Leap',1e2); %double wide is necessary, rest are generic values to deal with idealty
 p=scramble(p,'MatousekAffineOwen'); %Same. Cleans up some issues quickly and quietly
 
-candidateK=Kml+Kml*Kspan*(p(1:nCandidates,:)-.5); %Generate a sobol-distributed [0-1] set that theoretically spans the space very very well.
+candidateK=Kml+Kml*Kspan*(p(1:nCandidates,:)); %Generate a sobol-distributed [0-1] set that theoretically spans the space very very well.
 
 %You know you have some settling time, so figure out what it is.
 
-figure(2)
-clf
-subplot(2,1,1)
-hold on
-plot(t,Xd(:,1),'g','linewidth',2)
-
 Xml=reverse(t,Xh,F,Kml+K*0);
-plot(t,Xml(:,1),'r.')
 
 Xs=zeros(length(t),nCandidates);
 Ys=zeros(length(t),nCandidates);
@@ -76,185 +80,58 @@ for k=1:nCandidates
     cand{k}=reverse(t,Xh,F,candidateK(k)+K*0);
     Xs(:,k)=cand{k}(:,1);
     Ys(:,k)=cand{k}(:,2);
-    plot(t,Xs(:,k),'b')
+    %plot(t,Xs(:,k),'b')
 end
 
-eX=t;
-eY=t;
+eps=zeros(length(t),2);
 for k=1:length(t)
     temp=[candidateK 1+0*candidateK]\Xs(k,:)';
-    eX(k)=temp(1);
+    eps(k,1)=temp(1);
     temp=[candidateK 1+0*candidateK]\Ys(k,:)';
-    eY(k)=temp(1);
+    eps(k,2)=temp(1);
 end
 
-dim=1;
-P=zeros(dim);
-P(dim+1,dim+1)=1;
-w=zeros(2,1,length(t)+1);
-w(2,1,1)=Xh(1,1);
-w=[0;0];
+%% Try to find parameter-error component of intent
 
-subplot(2,1,2)
-hold on
+ws=zeros(4,2,length(t));
+
+P=eye(4);
+
+%w0=[(15-Kml)*eye(2);eye(2)];
+w0=[0*eye(2);eye(2)];
+
 for k=1:length(t)
-    %[w(:,:,k+1),P]=RLS([eX(k);1],Xml(k,1)',w(:,:,k),P,.5);
-    [w,P]=RLS([eX(k);1],Xml(k,1)',w,P,.9);
-    ws(:,k+1)=w;
-    if ~mod(k,20)
-    %plot(t(k),w(1,1,k),'r.')
-    plot(t(k),w,'r.')
-    drawnow
-    end
+    %[w,P]=RLS([eps(k,1);eps(k,2); 1],Xd(k,1:2),w,P,50);
+    alpha1(k,:)=Xd(k,1:2)-[eps(k,1);eps(k,2); Xml(k,1:2)']'*w0;
+    [w,P]=RLS([eps(k,1);eps(k,2); Xml(k,1:2)'],Xd(k,1:2),w,P,1.2);
+    alpha2(k,:)=Xd(k,1:2)-[eps(k,1);eps(k,2); Xml(k,1:2)']'*w;
+    ws(:,:,k)=w;
 end
-
-figure(3)
-clf
-subplot(2,1,1)
-hold on
-plot(t,Xd(:,1),'g-')
-plot(t,Xml(:,1)-eX','c.')
-%plot(t,squeeze(w(2,1,2:end)),'b.')
-subplot(2,1,2)
-%plot(t,squeeze(w(1,1,2:end)).*eX','r')
-
-
-%% Break there
-
-return
+alpha1-alpha2
 
 figure(2)
 clf
-subplot(2,1,1)
+
+subplot(3,1,1)
 hold on
-plot(t,Xd(:,1),'g')
-%arrow([t' Xd(:,1)],[t' Xd(:,1)]+.05*[0*t' F(:,1)],.6*[1 1 1],.2);
+plot(Xd(:,1),Xd(:,2),'g','linewidth',2)
+plot(Xh(:,1),Xh(:,2),'k','linewidth',2)
+plot(Xml(:,1)-(Kml-15)*eps(:,1),Xml(:,2)-(Kml-15)*eps(:,2),'k-')
+plot(squeeze(ws(3,1,:)),squeeze(ws(4,2,:)),'r.')
+axis equal
 
-Xh=forward(t,Xd,F,K);
-plot(t,Xh(:,1),'k')
-
-Klow=13;
-Khigh=16;
-
-XxL=reverse(t,Xh,F,Klow+0*K);
-plot(t,XxL(:,1),'r')
-
-XxH=reverse(t,Xh,F,Khigh+0*K);
-plot(t,XxH(:,1),'r')
-title('You can recover the twue intent by overestimation correction')
-ylabel('Displacement in X')
-xlim([0 1.5])
-legend('Ground Truth Intention','Hand (Stiffness=15)','Extraction with overestimated stiffness (Stiffness=18)','Extraction with overestimated stiffness (Stiffness=19)')
-
-subplot(2,1,2)
+subplot(3,1,2)
 hold on
-E=XxL(:,1)-XxH(:,1);
-plot(t,E,'c')
+plot(t,Xd(:,1),'g','linewidth',2)
+plot(t,Xh(:,1),'k','linewidth',2)
+plot(t,Xml(:,1)-(Kml-15)*eps(:,1),'k-')
+plot(t,squeeze(ws(3,1,:)),'r.')
 
-plot(t,Xd(:,1),'g')
-eps=E\XxL(:,1)
-plot(t,XxL(:,1)-eps*E,'r')
-legend('Difference of Overestimates','Ground Truth Intention','Corrected Extraction')
-title(['Solved Stiffness Overestimation: Eps = ',num2str(-eps*(Khigh-Klow))])
-xlabel('Time, Seconds')
-ylabel('Displacement in X')
-
-
-%% Time varying K
-
-Ksmooth=40+5*sin(2*pi*t/.25+rand)'; %smooth(5+20*rand(length(t),1),30);
-Xh=forward(t,Xd,F,[Ksmooth K(:,2)]);
-Klow=9;
-Kmid=12;
-Khigh=12.1;
-
-XxL=reverse(t,Xh,F,Klow+0*K);
-XxM=reverse(t,Xh,F,Kmid+0*K);
-XxH=reverse(t,Xh,F,Khigh+0*K);
-
-E=XxH(:,1)-XxL(:,1);
-
-span=2;
-epslocal=zeros(length(t),1);
-for k=1:length(t)
-    inds=max(1,k-span):length(t);
-    %Without zero mean, you lose the nuts for the trees
-    %epslocal(k)=(E(inds)-mean(E(inds)))\(XxM(inds,1)-mean(XxM(inds,1)));
-    temp=[E(inds) ones(length(inds),1)]\XxM(inds,1);
-    epslocal(k)=temp(1);
+subplot(3,1,3)
+hold on
+for k=2:2
+    for kk=1:2
+        plot(t,squeeze(ws(k,kk,:)),'r')
+    end
 end
-%epslocal=(epslocal(:,1)*Khigh+epslocal(:,2)*Klow)./sum(epslocal,2)
-
-realdot=dot(XxM(:,1),Xd(:,1)/norm(Xd(:,1)))
-epsdot=dot(XxL(:,1),E)
-epsglobal=E\XxM(:,1)
-
-epsComp=(XxL(:,1)-mean(XxL(:,1)))\(E-mean(E))
-
-
-figure(3)
-clf
-subplot(2,1,1)
-hold on
-
-plot(t,Xd(:,1),'g')
-plot(t,Xh(:,1),'k')
-plot(t,Ksmooth/10,'m')
-
-plot(t,XxL(:,1),'ro','markersize',2)
-plot(t,XxM(:,1),'rv','markersize',2)
-plot(t,XxH(:,1),'r^','markersize',2)
-plot(t,XxL(:,1)+epsComp*E,'color',[.8 .5 .2])
-plot(t,XxL(:,1)+epslocal.*E,'color',[0 .2 .6])
-
-title('You can recover the twue intent by overestimation correction')
-ylabel('Displacement in X')
-xlim([0 1.5])
-legend('Ground Truth Intention','Hand (Stiffness=magenta)','Time-varying Stiffness/10',['Extraction with estimated stiffness (Stiffness=',num2str(Klow),')'],['Extraction with estimated stiffness (Stiffness=',num2str(Kmid),')'],['Extraction with estimated stiffness (Stiffness=',num2str(Khigh),')'],'Constant Corrected Stiffness','Locally Corrected Stiffness')
-
-subplot(2,1,2)
-hold on
-
-%plot(t,epsglobal+0*t,'color',[.8 .5 .2])
-%plot(t,epslocal,'color',[0 .2 .6])
-plot(t,Xd(:,1)-Xh(:,1),'g')
-plot(t,10*(XxH(:,1)-XxM(:,1)),'r')
-
-%plot(t,epslocal(:,1),'b')
-%plot(t,(XxM(:,1)-Xd(:,1)),'color',[.7 .4 .4])
-%legend('Difference of Estimates','Ground Truth Intention','Constant-Corrected Extraction','Time-varying Stiffness Estimate Extraction')
-title(['Solved Stiffness Overestimation: Eps = ',num2str(epsglobal*(Khigh-Klow))])
-xlabel('Time, Seconds')
-ylabel('Computed scale')
-
-figure(4)
-clf
-hold on
-%plot(t,epsdot*E,'k')
-%plot(t,(XxM(:,1)-Xd(:,1)),'color',[.7 .4 .4])
-plot(t,0*t,'g')
-plot(t,Xh(:,1)-Xd(:,1),'k')
-plot(t,XxL(:,1)-Xd(:,1),'ro','markersize',2)
-plot(t,XxM(:,1)-Xd(:,1),'rv','markersize',2)
-plot(t,XxH(:,1)-Xd(:,1),'r^','markersize',2)
-plot(t,XxH(:,1)-XxL(:,1),'c')
-plot(t,XxL(:,1)+epsComp*E-Xd(:,1),'r')
-plot(t,XxM(:,1)-epslocal.*E-Xd(:,1),'color',[0 .2 .6])
-
-eh=XxL(:,1)-Xh(:,1);
-he=E\eh
-
-%plot(t,XxL(:,1)+he*E-Xd(:,1),'color',[.5 .1 .8])
-
-
-legend({'Zero Error';
-    'Hand Error';
-    ['Extraction Error (Stiffness=',num2str(Klow),')'];
-    ['Extraction Error (Stiffness=',num2str(Kmid),')'];
-    ['Extraction Error (Stiffness=',num2str(Khigh),')'];
-    'Unscaled Error Estimate';
-    'Correction with Linear Regression';
-    ['Correction with Local Linear Regression, Span = ',num2str(span)]})
-xlabel('Time, sec')
-ylabel('Error, X')
-
+ylim([-5 5])
