33ac38cfbbae356cd414aff6eeb634043282f112
Justin Horowitz <justin.horowitz@gmail.com>
2014-09-22 11:37:38 -0500

    No clue if this is even useful

--- Paper2/LMSExtract/RLS.m
+++ Paper2/LMSExtract/RLS.m
@@ -1,7 +1,7 @@
 function [w,p]=RLS(x,y,wm1,pm1,lambda)
 
-k=(lambda*pm1*x)*(1+lambda*x'*pm1*x)^-1;
-alpha=y-x'*wm1;
+k=(lambda*pm1*x)/(1+lambda*x'*pm1*x);
+alpha=(y-x'*wm1)';
 
 w=wm1+k*alpha;
 p=lambda*pm1-lambda*k*x'*pm1;
