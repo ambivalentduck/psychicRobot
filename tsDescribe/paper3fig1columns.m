17b552183c8a259a9b5f89eb4fc91243d767417c
Justin Horowitz <justin.horowitz@gmail.com>
2015-09-28 11:07:00 -0500

    updated for e vs cart sim

--- tsDescribe/paper3fig1columns.m
+++ tsDescribe/paper3fig1columns.m
@@ -5,8 +5,6 @@ font='Arial';
 fontsize=12;
 axlabsize=10;
 interpreter='tex';
-textInvariant=['\fontname{',font,'} \fontsize{',num2str(fontsize),'} '];
-axlabInvariant=['\fontname{',font,'} \fontsize{',num2str(axlabsize),'} '];
 
 figure(1)
 clf
@@ -40,7 +38,7 @@ neg=-empiricalP(empiricalP<=0);
 [Fn,xn,ln,un]=ecdf(neg);
 
 h=fill([p; wrev(p)],[expcdf(p,ci(1)); expcdf(wrev(p),ci(2))],fitcolor);
-set(h,'edgealpha',0,'facealpha',.5)
+set(h,'edgealpha',0,'facealpha',.5);
 h=fill([xp(~isnan(lp)); wrev(xp(~isnan(up)))],[lp(~isnan(lp)); wrev(up(~isnan(up)))],poscolor);
 set(h,'edgealpha',0,'facealpha',.5)
 h=fill([xn(~isnan(ln)); wrev(xn(~isnan(un)))],[ln(~isnan(ln)); wrev(un(~isnan(un)))],negcolor);
@@ -104,7 +102,6 @@ for k=1:length(t1)
     ta=((t(inds)-tc))/ts+.5;
     kern=(x2(k)-x1(k))*(30*ta.^2-60*ta.^3+30*ta.^4)/ts;
     acc=(x2(k)-x1(k))*(60*ta-180*ta.^2+120*ta.^3)/(ts^2);
-    any(acc<0)
     
     subs(k).inds=inds; %#ok<*SAGROW>
     subs(k).t=t(inds);
@@ -153,8 +150,7 @@ text(.13,.8,'\{','rotation',-90,'horizontalalignment','center','verticalalignmen
 text(.128,1.15,'1','horizontalalignment','center','fontsize',12)
 text(.27,-.9,'\{','rotation',90,'horizontalalignment','center','verticalalignment','middle','fontsize',24)
 text(.28,-1.3,'2','horizontalalignment','center','fontsize',12)
-legend([hlump,henergy,hcart],{'Subunit','$\sum$ Energy','$\sum$ Cartesian'},'interpreter','latex')
-
+legend([hlump,henergy,hcart],{'Subunit','\Sigma Energy','\Sigma Velocity'})
 
 %Energy
 spHandles(4)=subplot(Nsub,Msub,4);
@@ -173,21 +169,95 @@ set(gca,'xtick',[0 1])
 ylabel('Energy, Joules')
 xlabel('Time, seconds')
 
+% Velocity
+spHandles(6)=subplot(Nsub,Msub,6)
+hold on
+plot(t,cartV,'-','color',cartColor,'linewidth',totalWidth)
+plot(t,energyV,'-','color',energyColor,'linewidth',totalWidth)
+resid=energyV;
+for k=1:length(subs)
+    plot(subs(k).t,subs(k).v,'-','color',subColor)
+    resid(subs(k).inds)=resid(subs(k).inds)-subs(k).v;
+end
+plot(t,abs(resid),'.','color',energyColor,'markersize',residSize)
+ylabel('Velocity, meters/second')
+xlabel('Time, seconds')
+
+%% Quick sim
+N=floor(10*60/.3);
+L2Tn2=exprnd(.07,N,1);
+Tn2=.1+exprnd(.3,N,1);
+dx=L2Tn2./Tn2;
+t1=zeros(N,1);
+t2=t1;
+for k=1:N
+   t2(k)=t1(k)+1/sqrt(Tn2(k));
+   xp=.2; %+exprnd(.3);
+   if xp>.9
+       xp=.9;
+   end
+   if k~=N
+       t1(k+1)=t1(k)+xp*(t2(k)-t1(k));
+   end
+end
+t=0:.005:t2(end);
+vtotal=zeros(size(t));
+atotal=zeros(size(t));
+etotal=zeros(size(t));
+ptotal=zeros(size(t));
+
+for k=1:N
+    inds=find((t>=t1(k))&(t<=t2(k)));
+    tc=(t1(k)+t2(k))/2;
+    ts=t2(k)-t1(k);
+    ta=((t(inds)-tc))/ts+.5;
+    kern=dx(k)*(30*ta.^2-60*ta.^3+30*ta.^4)/ts;
+    acc=dx(k)*(60*ta-180*ta.^2+120*ta.^3)/(ts^2);
+    
+    % Cartesian superposition
+    vtotal(inds)=vtotal(inds)+kern;
+    atotal(inds)=atotal(inds)+acc;
+    
+    % Energy superposition
+    ptotal(inds)=ptotal(inds)+mass*kern.*acc;
+    etotal(inds)=etotal(inds)+.5*mass*kern.^2;
+end
+cartPm=mass*atotal.*vtotal;
+cartEm=.5*mass*vtotal.^2;
+cartVm=vtotal;
+
+energyPm=ptotal;
+energyEm=etotal;
+energyVm=sqrt(2*etotal/mass);
+
+spHandles(5)=subplot(Nsub,Msub,5);
+hold on
+empiricalE=mass*dot(v',v');
+%[Fe,xe,le,ue]=ecdf(empiricalE);
+[Fcm,xcm,lcm,ucm]=ecdf(cartEm);
+[Fem,xem,lem,uem]=ecdf(energyEm);
+plot(xe,Fe,'b')
+plot(xem,Fem,'r')
+plot(xcm,Fcm,'g')
+xlim([-.1 .5])
+
 %% Cleanup at the end
+
+%Setup axes
 allaxes=findobj(gcf,'type','axes');
-set(allaxes,'linewidth',2,'ticklength',[0;0])
+set(allaxes,'linewidth',2,'ticklength',[0;0]);
 set(gcf,'units','inches')
 figW=8.5;
-figH=11;
-set(gcf,'position',[8 5 figW figH])
+figH=11/2;
+set(gcf,'position',[8 5 figW figH]);
 
 lmargin=.08;
 rmargin=.02;
 tmargin=.02;
 bmargin=.02;
-rowHeights=[.2 .6 .2];
-rowMargins=[0 .1 .1];
-rowBottoms=1-tmargin-cumsum(rowHeights+rowMargins);
+rowHeights=[.25 .25 .25];
+rowMargins=[0 .08 .08];
+rowBottoms=1-cumsum(rowHeights+rowMargins);
 colWidth=.38;
 colX=[lmargin 1-rmargin-colWidth];
 for k=1:length(spHandles)
@@ -196,6 +266,16 @@ for k=1:length(spHandles)
     end
     column=2-mod(k,2);
     row=floor((k-1)/2)+1;
-    set(spHandles(k),'position',[colX(column) rowBottoms(row) colWidth rowHeights(row)])
-    
+    set(spHandles(k),'position',[colX(column) rowBottoms(row) colWidth rowHeights(row)]);
 end
+
+%Format all text
+alltext=findobj(gcf,'type','text');
+
+textInvariant=['\fontname{',font,'} \fontsize{',num2str(fontsize),'} '];
+for k=1:length(alltext)
+    string=get(alltext(k),'string');
+    set(alltext(k),'string',[textInvariant, string],'interpreter',interpreter);
+end
+
+
